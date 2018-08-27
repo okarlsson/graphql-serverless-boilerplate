04c6a138109d19c10de160d250252f0beb36b7a6
Oskar Karlsson <oskarkarlsson1993@gmail.com>
2018-08-27 22:31:21 +0200

    :lipstick:

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,7 @@
-import express from "express";
-import serverless from "serverless-http";
-import graphiql from "graphql-playground-middleware-express";
-import { ApolloServer } from "apollo-server-express";
+import express from 'express';
+import serverless from 'serverless-http';
+import graphiql from 'graphql-playground-middleware-express';
+import { ApolloServer } from 'apollo-server-express';
 import { typeDefs } from './types/hello';
 import resolvers  from './resolvers/hello';
 
@@ -10,10 +10,11 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  path: "/graphql"
+  path: '/graphql'
 });
 
 server.applyMiddleware({ app });
-app.get("/playground", graphiql({ endpoint: "/graphql" }));
+app.get('/playground', graphiql({ endpoint: '/graphql' }));
+
 const handler = serverless(app);
 export { handler };
